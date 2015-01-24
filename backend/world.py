e6cf3c8d88b9af6a7aaf890da5d014574d9a29f8
Elliot Hughes <elliot.hughes@gmail.com>
2015-01-24 18:35:40 +0000

    extract method

--- backend/world.py
+++ backend/world.py
@@ -50,11 +50,13 @@ class World:
     def add_player(self, player):
         if player in self.player_locations:
             raise ValueError("Player already in world")
+        self.set_player_location(player)
 
+    def set_player_location(self, player):
         if len(self.start_tile_positions) > 0:
             self.player_locations[player] = random.choice(self.start_tile_positions) 
         else: 
-            self.player_locations[player = (100, 100)
+            self.player_locations[player] = (100, 100)
 
     def remove_player(self, player):
         if player in self.player_locations:
