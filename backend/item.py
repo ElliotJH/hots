93a9912a99557a4e7d8644bdb7b46a4e1fcda297
Dan Palmer <dan.palmer@me.com>
2015-01-24 20:17:25 +0000

    Item eq/hash

--- backend/item.py
+++ backend/item.py
@@ -1,5 +1,13 @@
 class Item:
 
     def __init__(self, item_id):
-        super().__init__()
         self.item_id = item_id
+
+    def __hash__(self):
+        return hash(self.item_id)
+
+    def __eq__(self, other):
+        if not isinstance(other, Item):
+            return False
+
+        return self.item_id == other.item_id
