962d822b9d1d8cad4044c944a3bc1973cb2d190b
Dan Palmer <dan.palmer@me.com>
2015-01-24 13:04:05 +0000

    World should describe itself

--- backend/world.py
+++ backend/world.py
@@ -8,3 +8,6 @@ class World(object):
     def load(self, fname):
         with open(fname, 'r') as f:
             self.tiles = [[int(i) for i in line if i != '\n'] for line in f]
+
+    def serialise(self):
+        return {'world': self.tiles}
