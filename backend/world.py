d3ca3a8f0d650e5950b21cd6ce38b6caa4d85e06
Dan Palmer <dan.palmer@me.com>
2015-01-24 16:22:57 +0000

    Merge Elliot's code

--- backend/world.py
+++ backend/world.py
@@ -1,11 +1,14 @@
+import math
+
+
 class World:
 
-    player_locations = {}
-
-    def __init__(self):
+    def __init__(self, tile_size=20):
         super().__init__()
         self.items = []
         self.tiles = None
+        self.tile_size = tile_size
+        self.player_locations = {}
 
     def load(self, fname):
         with open(fname, 'r') as f:
@@ -20,6 +23,58 @@ class World:
     def remove_player(self, player):
         if player in self.player_locations:
             del self.player_locations[player]
+
+    @staticmethod
+    def collides(pos1, rad1, pos2, rad2):
+        """Test for rectangular collisions for the demo"""
+        x1 = pos1[0]
+        y1 = pos1[1]
+
+        x2 = pos2[0]
+        y2 = pos2[1]
+
+        # Right edge is over the left edge and
+        # Left edge is not over the right edge
+        horizontal_colliding = (
+            ((x1 + rad1) > (x2 - rad2)) and ((x1 - rad1) < (x2 + rad2))
+        )
+        vertical_colliding = (
+            ((y1 + rad1) > (y2 - rad2)) and ((y1 - rad1) < (y2 + rad2))
+        )
+
+        return horizontal_colliding and vertical_colliding
+
+    def move_player(self, player, direction, distance=10):
+        angle = {
+            'up': 0, 'down': 180, 'left': 270, 'right': 90
+        }[direction]
+
+        old_position = self.players[player]
+        new_x = distance * math.sin(angle) + old_position[0]
+        new_y = distance * math.cos(angle) + old_position[1]
+
+        new_position = (new_x, new_y)
+
+        position = self.move(old_position, new_position)
+        self.players[player] = position
+
+        return position
+
+    def attempt_move(self, old_position, new_position, player_radius=20):
+        # This is massively bugged - if the player tries to move through an
+        # object then we're just fine with that.
+
+        # Massively inefficient, we don't need to check many of these at all
+        for (row_num, columns) in enumerate(self.tiles):
+            for (col_num, cell) in enumerate(columns):
+                if self.collides(
+                    new_position,
+                    player_radius,
+                    (col_num, row_num),
+                    self.tile_width,
+                ):
+                    return old_position
+        return new_position
 
     # Serialisation to structures that can be JSON'd
 
