2c6182c35bb9517ec7a6c284a5debe18982893b6
Dan Palmer <dan.palmer@me.com>
2015-01-24 18:49:11 +0000

    Make this an even vaguely reasonable python style

--- backend/constant_objects.py
+++ backend/constant_objects.py
@@ -1,62 +1,63 @@
 levels = {
-    'ship' : 36,
+    'ship': 36,
     'spaceship': 37,
     'plane': 38,
     'tropical_island': 39,
     'arctic': 40,
-    'desert': 41
+    'desert': 41,
 }
+
 weapons = 42
 
 game_objects = {
-    levels['ship'] : {
-        'life_ring' : 0,
-        'rope' : 1,
-        'raincoat' : 2,
-        'sextant' : 3,
-        'anchor' : 4
+    levels['ship']: {
+        'life_ring': 0,
+        'rope': 1,
+        'raincoat': 2,
+        'sextant': 3,
+        'anchor': 4,
     },
-    levels['spaceship'] : {
-        'space_helmet' : 5,
-        'o2_tank' : 6,
-        'jetpack' : 7,
-        'alien' : 8,
-        'phaser' : 9
+    levels['spaceship']: {
+        'space_helmet': 5,
+        'o2_tank': 6,
+        'jetpack': 7,
+        'alien': 8,
+        'phaser': 9,
     },
-    levels['plane'] : {
-        'parachute' : 10,
-        'landing_gear' : 11,
-        'breathing_mask' : 12,
-        'suitcase' : 13,
-        'passport' : 14
+    levels['plane']: {
+        'parachute': 10,
+        'landing_gear': 11,
+        'breathing_mask': 12,
+        'suitcase': 13,
+        'passport': 14,
     },
-    levels['tropical_island'] : {
-        'cocktail' : 15,
-        'beachball' : 16,
-        'trunks' : 17,
-        'desert_island_disc' : 18,
-        'pineapple' : 19
+    levels['tropical_island']: {
+        'cocktail': 15,
+        'beachball': 16,
+        'trunks': 17,
+        'desert_island_disc': 18,
+        'pineapple': 19,
     },
-    levels['arctic'] : {
-        'penguin' : 20,
-        'snowman' : 21,
-        'pole' : 22,
-        'santa' : 23,
-        'scarf' : 24
+    levels['arctic']: {
+        'penguin': 20,
+        'snowman': 21,
+        'pole': 22,
+        'santa': 23,
+        'scarf': 24,
     },
-    levels['desert'] : {
-        'palm_tree' : 25,
-        'camel' : 26,
-        'water_bottle' : 27,
-        'bucket_' : 28,
-        'dune_buggy' : 29
+    levels['desert']: {
+        'palm_tree': 25,
+        'camel': 26,
+        'water_bottle': 27,
+        'bucket_': 28,
+        'dune_buggy': 29,
     },
-    weapons : {
-        'start_with_fists' : 30,
-        'spoon' : 31,
-        'gun' : 32,
-        'knife' : 33,
-        'board_with_a_nail_in_it' : 34,
-        'taser' : 35
-    }
+    weapons: {
+        'start_with_fists': 30,
+        'spoon': 31,
+        'gun': 32,
+        'knife': 33,
+        'board_with_a_nail_in_it': 34,
+        'taser': 35,
+    },
 }
