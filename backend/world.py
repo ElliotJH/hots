94017c0c9c5a036d75a365235fa44c7a45db0cf4
Elliot Hughes <elliot.hughes@gmail.com>
2015-01-25 11:48:36 +0000

    commit working code

--- backend/world.py
+++ backend/world.py
@@ -286,19 +286,19 @@ class World:
         return (left, right, top, bottom)
 
     def attempt_move(self, old_position, new_position, object_radius=0, blocked=[1, 2], winning=[]):
-        xIsh = math.round(new_position[0] / GRID_SIZE)
-        yIsh = math.round(new_position[1] / GRID_SIZE)
+        xIsh = round(new_position[0] / GRID_SIZE)
+        yIsh = round(new_position[1] / GRID_SIZE)
 
         testSquares = [[xIsh, yIsh], [xIsh-1, yIsh], [xIsh - 1, yIsh - 1], [xIsh, yIsh - 1],
             [xIsh+1, yIsh -1], [xIsh+1, yIsh], [xIsh + 1, yIsh + 1], [xIsh, yIsh+1], [xIsh-1, yIsh+1]]
 
         for square in testSquares:
-            cell = self.tiles[square[0]][square[1]]
+            cell = self.tiles[square[1]][square[0]]
             if cell in blocked or cell in winning:
                 cell_square = collisions.Square(*self.grid_to_centered_point(square[1], square[0]))
                 player_circle = collisions.Circle(new_position[0], new_position[1], GRID_SIZE*2/3)
 
-                col = collisions.circle_square(player_circle, cell_square):
+                col = collisions.circle_square(player_circle, cell_square)
                 if col:
                     if cell in winning:
                         raise WonException
